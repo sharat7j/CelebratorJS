dc500a3e4b22992dc121c333469ab2081c2fab27
sharat7j <sharsmail@gmail.com>
2012-04-13 14:36:02 -0500

    Update pro001.js

--- pro001.js
+++ pro001.js
@@ -15,8 +15,7 @@
  * But it doesn't seem to.  I took the shortcut of using window.innerHeight
  * and window.pageYOffset, and I'm guess that is what is breaking it.
  * 
- * Copyright 2010 by David Flanagan
- * http://creativecommons.org/licenses/by-nc-sa/3.0/
+ 
  */
 (function() {
     // If we don't have a working <canvas> element, apologize and quit
